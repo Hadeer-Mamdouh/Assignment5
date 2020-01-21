dd6707047df0decd136ca7b1616be3d0bafad2ca
Hadeer-Mamdouh <59342982+Hadeer-Mamdouh@users.noreply.github.com>
2020-01-21 09:26:54 +0300

    Update assi8.php

--- assi8.php
+++ assi8.php
@@ -1,5 +1,5 @@
 <form action="handelassi8.php" method="post">
-    <input type="number" name="input">
+    <input type="number" name="input" min="100" max="5000">
     
     <input type="submit" value="Search">
     
