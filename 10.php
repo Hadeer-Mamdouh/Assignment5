3b0f5e6209ce55c44bc6d6946d7ffce5092aef06
Hadeer-Mamdouh <59342982+Hadeer-Mamdouh@users.noreply.github.com>
2020-01-22 19:28:26 +0300

    Update 10.php

--- 10.php
+++ 10.php
@@ -8,7 +8,7 @@ $dbuser="root";
 $dbpassword="";
 $dbname="rout-company";   // check your DB name
 
-
+if(isset($_SESSION['mail'])){
 $conn= new mysqli ($servername,$dbuser,$dbpassword,$dbname);
 
 
@@ -16,8 +16,7 @@ if($conn->connect_error)
 
 die("not connect :".$conn->connect_error);
 else
-echo "connected success"."<br>";
-
+{
 
 $id=$_GET["id"];
 
@@ -67,4 +66,11 @@ else
     echo "The the product ID is  not found";
 
 
- 
+}
+
+else {
+    
+    	echo " Go <a href='login.php'>Login</a> <br/>";
+
+}
+ ?>
