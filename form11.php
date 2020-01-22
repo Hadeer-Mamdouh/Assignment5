d5504e9f4213eae7b07031083e256121f730a656
Hadeer-Mamdouh <59342982+Hadeer-Mamdouh@users.noreply.github.com>
2020-01-22 20:50:03 +0300

    Update form11.php

--- form11.php
+++ form11.php
@@ -1,8 +1,8 @@
 <form action="form_index.php"  method="post" >
-    Email : <input type="email" name="email" placeholder="Enter Your Email">
+    Email : <input type="email" name="mail" placeholder="Enter Your Email">
     <br>
     <br>
-    Password : <input type="password" name="password" placeholder="Enter Your Password" >
+    Password : <input type="password" name="pass" placeholder="Enter Your Password" >
     <br>
     <br>
     <input type="submit"  value="Login">
