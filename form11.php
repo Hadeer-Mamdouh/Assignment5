a74eee732aae6c3b1a7ec44fad1d1a056ce87d83
khforever <59407743+khforever@users.noreply.github.com>
2020-01-22 09:26:25 +0200

    Create form11.php

---
+++ form11.php
@@ -0,0 +1,10 @@
+<form action="form_index.php"  method="post" >
+    Email : <input type="email" name="email" placeholder="Enter Your Email">
+    <br>
+    <br>
+    Password : <input type="password" name="password" placeholder="Enter Your Password" >
+    <br>
+    <br>
+    <input type="submit"  value="Login">
+
+</form>
